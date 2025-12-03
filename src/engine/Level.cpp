4d4271b8958b498c83ebbbfd7c1a50e44b0996d9
aparera <aparera@sasa-mexico.com>
2025-12-03 22:55:24 +1000

    se crea la clase level, donde se carga el fondo y las plataformas de este primer nivel

---
+++ src/engine/Level.cpp
@@ -0,0 +1,51 @@
+#include "Level.h"
+#include "ResourceManager.h" // gestor de assets
+
+Level::Level(Renderer* renderer) {
+    // se obtienen los recursos necesarios
+    SDL_Renderer* rawRen = renderer->getRenderer();
+    auto& res = ResourceManager::getInstance();
+
+    // se cargan las texturas
+    background = res.loadTexture("assets/fondo.png", rawRen);
+    platBigTex = res.loadTexture("assets/plataformaGrande.png", rawRen);
+    platSmallTex = res.loadTexture("assets/platafomaPequena.png", rawRen);
+
+    // se define la FÍSICA del nivel (Las "Cajas Rojas")
+    // aquí definimos dónde chocará el jugador.
+    
+    // plataforma Grande (X: 55, Y: 65)
+    Platform p1 = {55, 65, 250, 100};
+    platforms.push_back(p1);
+
+    // plataforma Pequeña (X: 400, Y: 310)
+    Platform p2 = {400, 310, 100, 80};
+    platforms.push_back(p2);
+}
+
+void Level::render(Renderer* renderer) {
+    // se dibuja el fondo (Siempre primero)
+    if (background) {
+        renderer->drawTexture(background, 0, 0, 800, 600, false);
+    }
+
+    // se dibujan las plataformas (-10 en Y para que quede al tiro)
+
+    // se dibuja la primera plataforma
+    if (platforms.size() > 0 && platBigTex) {
+        // La física dice Y=65, dibujamos en Y=55 (65 - 10)
+        // La altura visual es 120 (más alta que la física para las raíces colgando)
+        renderer->drawTexture(platBigTex, platforms[0].x, platforms[0].y - 10, platforms[0].w, 120, false);
+    }
+
+    // se dibuja la segunda plataforma (Pequeña)
+    if (platforms.size() > 1 && platSmallTex) {
+        // La física dice Y=310, dibujamos en Y=300 (310 - 10)
+        renderer->drawTexture(platSmallTex, platforms[1].x, platforms[1].y - 10, platforms[1].w, 100, false);
+    }
+
+    // NOTA PARA EL FUTURO:
+    // Si tuvieras 100 plataformas, no harías if(size > 0), if(size > 1)...
+    // Crearías una clase "Tile" o "PlatformObject" que guarde su propia textura.
+    // Pero para este nivel de 2 plataformas, esto es perfecto y eficiente.
+}
---
+++ src/engine/Level.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <vector>
+#include "Physics.h" // struct Platform
+#include "Renderer.h"
+
+class Level {
+public:
+    Level(Renderer* renderer);
+    void render(Renderer* renderer);
+    
+    // para que la física sepa contra qué chocar
+    const std::vector<Platform>& getPlatforms() const { return platforms; }
+
+private:
+    std::vector<Platform> platforms;
+    SDL_Texture* background;
+    SDL_Texture* platBigTex;
+    SDL_Texture* platSmallTex;
+};
